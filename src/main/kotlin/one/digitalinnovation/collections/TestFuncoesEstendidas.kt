ac553509eec2ecf56a706dede13e3e0dc2063122
luiz henrique <l.henrique.saraiva@gmail.com>
2021-03-05 11:35:45 -0300

    Created test extend function class

---
+++ src/main/kotlin/one/digitalinnovation/collections/TestFuncoesEstendidas.kt
@@ -0,0 +1,16 @@
+package one.digitalinnovation.collections
+
+fun main() {
+
+    val teste = arrayOf(
+        "1000".toBigDecimal(),
+        "20000".toBigDecimal()
+    )
+
+    println("---------------")
+    println(teste.somatoria())
+
+    println("---------------")
+    println(teste.media())
+
+}
